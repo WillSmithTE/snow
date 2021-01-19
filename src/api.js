af8a28809da61ac640a444fba8b313ec320f6f95
William Smith <williamsmith@Services-MacBook-Pro.local>
2021-01-20 08:44:55 +1100

    add logging

--- src/api.js
+++ src/api.js
@@ -1,10 +1,13 @@
-const BASE_URL = process.env.NODE_ENV === 'development' ? 
+const BASE_URL = process.env.NODE_ENV === 'development' ?
 'http://localhost:5000' : 'https://ski-be-hgfltltt5a-de.a.run.app'
 
 export const api = {
     getSpencersCreek: async () => {
+        console.error(1)
         const response = await fetch(`${BASE_URL}/api/spencersCreek/a`);
+        console.error(2)
         const jsonResponse = await response.json();
+        console.error(3)
         return jsonResponse;    
     }
 }
