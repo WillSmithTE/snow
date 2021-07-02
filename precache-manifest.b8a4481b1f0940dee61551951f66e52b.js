7fc18ad7ab03c5dc732a6c2ec0c8dbc503882fe0
Will Smith <willsmithte@gmail.com>
2021-07-02 15:09:38 +1000

    Updates

--- precache-manifest.b8a4481b1f0940dee61551951f66e52b.js
+++ precache-manifest.b8a4481b1f0940dee61551951f66e52b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "19dd3654cc31fce132cddfdc9fda4e6f",
+    "revision": "28f9ef21ba90f86cfbec4d27abc61651",
     "url": "/snow/index.html"
   },
   {
-    "revision": "188ae27da7ad8a5ca731",
+    "revision": "43ace3ebabb8dba3d335",
     "url": "/snow/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "ff23433d4aeaf06dc21c",
-    "url": "/snow/static/js/2.435d08d0.chunk.js"
+    "revision": "8af32914513a7f714d51",
+    "url": "/snow/static/js/2.27a76114.chunk.js"
   },
   {
     "revision": "556d72da1c19c1e88e2917fa6505d7f0",
-    "url": "/snow/static/js/2.435d08d0.chunk.js.LICENSE.txt"
+    "url": "/snow/static/js/2.27a76114.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "188ae27da7ad8a5ca731",
-    "url": "/snow/static/js/main.1d914a03.chunk.js"
+    "revision": "43ace3ebabb8dba3d335",
+    "url": "/snow/static/js/main.2552ccbb.chunk.js"
   },
   {
     "revision": "1777faf5c135a68f2ce7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/snow/precache-manifest.9a4e185a7f628db9053ce58ce9779c77.js"
+  "/snow/precache-manifest.b8a4481b1f0940dee61551951f66e52b.js"
 );
 
 self.addEventListener('message', (event) => {
