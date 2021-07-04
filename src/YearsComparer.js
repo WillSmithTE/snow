f6acedaf450b6f196b3cd25445eb12d9a194fd7f
Will Smith <willsmithte@gmail.com>
2021-07-04 14:54:07 +1000

    fix console errors

--- src/YearsComparer.js
+++ src/YearsComparer.js
@@ -20,8 +20,6 @@ export const YearsComparer = () => {
 
     const { data: placeData, place, isSouthernHemisphere } = (data && data[placeIndex]) || {};
 
-    console.error({ placeData, place, isSouthernHemisphere })
-
     return <>
         {error && <Error />}
         {data ?
@@ -38,9 +36,9 @@ export const YearsComparer = () => {
 };
 
 const PlaceSelector = ({ placeIndex, setPlaceIndex, data }) => {
-    return <FormControl style={{ width: '300px', padding: '15px' }}>
+    return <FormControl style={{ width: '300px', paddingTop: '15px 0px 20px' }}>
         <Select value={placeIndex} onChange={(e) => setPlaceIndex(e.target.value)}>
-            {data.map(({ place }, index) => <MenuItem value={index}>{place}</MenuItem>)}
+            {data.map(({ place }, index) => <MenuItem value={index} key={index}>{place}</MenuItem>)}
         </Select>
     </FormControl>
 };
