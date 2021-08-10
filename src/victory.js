4a4acddb4d405768926f0ee44748b2c54195ab7d
Will Smith <willsmithte@gmail.com>
2021-08-10 17:48:37 +1000

    refactor from class component to functional

--- src/victory.js
+++ src/victory.js
@@ -35,144 +35,113 @@ const months = [
     { firstDay: 366, south: "", north: "", both: "" },
 ];
 
-export default class InteractiveLegend extends React.Component {
+export const InteractiveLegend = ({ series, monthDisplay }) => {
 
-    constructor(props) {
-        super(props);
-        this.state = { hiddenSeries: new Set() };
-    }
+    const [events, setEvents] = React.useState([]);
 
-    buildEvents() {
-        return this.props.series.map((_, idx) => {
-            return {
-                childName: ['legend'],
-                target: ['data', 'labels'],
-                eventKey: String(idx),
-                eventHandlers: {
-                    onClick: () => {
-                        return [
-                            {
-                                childName: ['area-' + idx],
-                                target: 'data',
-                                eventKey: 'all',
-                                mutation: () => {
-                                    if (!this.state.hiddenSeries.delete(idx)) {
-                                        // Was not already hidden => add to set
-                                        this.state.hiddenSeries.add(idx);
+    React.useEffect(() => {
+        setEvents(
+            series.map((_, idx) => {
+                return {
+                    childName: ['legend'],
+                    target: ['data', 'labels'],
+                    eventKey: String(idx),
+                    eventHandlers: {
+                        onMouseOver: () => {
+                            return [
+                                {
+                                    childName: ['area-' + idx],
+                                    target: 'data',
+                                    eventKey: 'all',
+                                    mutation: props => {
+                                        return {
+                                            style: { ...props.style, strokeWidth: 4, fillOpacity: 0.5 }
+                                        };
                                     }
-                                    this.setState({ hiddenSeries: new Set(this.state.hiddenSeries) });
-                                    return null;
                                 }
-                            }
-                        ];
-                    },
-                    onMouseOver: () => {
-                        return [
-                            {
-                                childName: ['area-' + idx],
-                                target: 'data',
-                                eventKey: 'all',
-                                mutation: props => {
-                                    return {
-                                        style: { ...props.style, strokeWidth: 4, fillOpacity: 0.5 }
-                                    };
+                            ];
+                        },
+                        onMouseOut: () => {
+                            return [
+                                {
+                                    childName: ['area-' + idx],
+                                    target: 'data',
+                                    eventKey: 'all',
+                                    mutation: () => {
+                                        return null;
+                                    }
                                 }
-                            }
-                        ];
-                    },
-                    onMouseOut: () => {
-                        return [
-                            {
-                                childName: ['area-' + idx],
-                                target: 'data',
-                                eventKey: 'all',
-                                mutation: () => {
-                                    return null;
-                                }
-                            }
-                        ];
-                    }
-                }
-            };
-        });
-    }
-
-    render() {
-
-        return (
-            <div>
-                <VictoryChart
-                    height={200}
-                    events={this.buildEvents()}
-                    containerComponent={
-                        <VictoryVoronoiContainer
-                            labels={({ datum: { y } }) => `${y}cm`}
-                            radius={5}
-                            labelComponent={<VictoryTooltip
-                                centerOffset={{ x: 5 }}
-                                style={{ fontSize: '6px' }}
-                            />}
-                        />
-                    }
-                // domain={{ x: [1, 365], y: [0, 350] }}
-                >
-                    <VictoryAxis
-                        style={{
-                            grid: { stroke: "#818e99", strokeWidth: 0.5 },
-                            tickLabels: { fontSize: 8 },
-                        }}
-                        tickValues={months.map(({ firstDay }) => firstDay)}
-                        tickFormat={(day) => {
-                            const month = months.find(({ firstDay }) => firstDay === day);
-                            return month[this.props.monthDisplay];
-                        }}
-                    />
-                    <VictoryAxis
-                        style={{
-                            grid: { stroke: "#818e99", strokeWidth: 0.5 },
-                            tickLabels: { fontSize: 8 },
-                        }}
-                        dependentAxis={true} tickFormat={(snow) => `${snow}cm`}
-
-                    />
-                    {this.props.series.map((s, idx) => {
-                        if (this.state.hiddenSeries.has(idx)) {
-                            return undefined;
+                            ];
                         }
-                        return (
-                            <VictoryArea
-                                key={'area-' + idx}
-                                name={'area-' + idx}
-                                data={toVictoryData(s)}
-                                style={{
-                                    data: {
-                                        fill: s.color,
-                                        fillOpacity: 0.2,
-                                        stroke: s.color,
-                                        strokeWidth: 2
-                                    }
-                                }}
-                            />
-                        );
-                    })}
-                    <VictoryLegend
-                        orientation='horizontal'
-                        itemsPerRow={6}
-                        name={'legend'}
-                        data={this.props.series.map((s, idx) => {
-                            const item = toVictoryLegend(s);
-                            if (this.state.hiddenSeries.has(idx)) {
-                                return { ...item, symbol: { fill: '#999' } };
-                            }
-                            return item;
-                        })}
-                        height={10}
-                        style={{
-                            labels: { fontSize: 8, cursor: 'pointer' }
-                        }}
+                    }
+                };
+            })
+        )
+    }, [series]);
+
+    return (
+        <div>
+            <VictoryChart
+                height={200}
+                events={events}
+                containerComponent={
+                    <VictoryVoronoiContainer
+                        labels={({ datum: { y } }) => `${y}cm`}
+                        radius={5}
+                        labelComponent={<VictoryTooltip
+                            centerOffset={{ x: 5 }}
+                            style={{ fontSize: '6px' }}
+                        />}
                     />
-                </VictoryChart>
-            </div>
-        );
-    }
+                }
+            >
+                <VictoryAxis
+                    style={{
+                        grid: { stroke: "#818e99", strokeWidth: 0.5 },
+                        tickLabels: { fontSize: 8 },
+                    }}
+                    tickValues={months.map(({ firstDay }) => firstDay)}
+                    tickFormat={(day) => {
+                        const month = months.find(({ firstDay }) => firstDay === day);
+                        return month[monthDisplay];
+                    }}
+                />
+                <VictoryAxis
+                    style={{
+                        grid: { stroke: "#818e99", strokeWidth: 0.5 },
+                        tickLabels: { fontSize: 8 },
+                    }}
+                    dependentAxis={true} tickFormat={(snow) => `${snow}cm`}
+
+                />
+                {series.map((s, idx) => {
+                    return (
+                        <VictoryArea
+                            key={'area-' + idx}
+                            name={'area-' + idx}
+                            data={toVictoryData(s)}
+                            style={{
+                                data: {
+                                    fill: s.color,
+                                    fillOpacity: 0.2,
+                                    stroke: s.color,
+                                    strokeWidth: 2
+                                }
+                            }}
+                        />
+                    );
+                })}
+                <VictoryLegend
+                    orientation='horizontal'
+                    itemsPerRow={6}
+                    name={'legend'}
+                    data={series.map((s, idx) => toVictoryLegend(s))}
+                    height={50}
+                    style={{ labels: { fontSize: 8, cursor: 'pointer' } }}
+                />
+            </VictoryChart>
+        </div>
+    );
 }
+
+export default InteractiveLegend;
